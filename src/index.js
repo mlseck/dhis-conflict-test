5b9498eacb65cb206c733b06e4f004218666815b
“Adja <m.seck@melotek.co>
2023-04-13 12:28:56 +0000

    Testing rendering of index page

--- src/index.js
+++ src/index.js
@@ -1,7 +1,41 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { init } from 'd2'
+import axios from 'axios'
+import './index.css'
+import App from './App'
+import dotenv from 'dotenv'
 
-const myFirstElement = <h1>Hello React!</h1>
+import 'typeface-roboto'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(myFirstElement);
+dotenv.config()
+const appName = process.env.REACT_APP_DHIS2_APP_NAME
+const apiVersion = process.env.REACT_APP_DHIS2_API_VERSION
+
+export async function getBaseUrl() {
+    console.info('Rendering index')
+    if (process.env.NODE_ENV === "development") {
+        const baseUrl = process.env.REACT_APP_DHIS2_BASE_URL || "http://localhost:8080"
+        console.info(`[DEV] DHIS2 instance: ${baseUrl}`)
+        return baseUrl.replace(/\/*$/, "")
+    } else {
+        const { data: manifest } = await axios.get("manifest.webapp")
+        return manifest.activities.dhis.href
+    }
+}
+async function main() {
+    const baseUrl = await getBaseUrl()
+    init({ baseUrl: baseUrl + '/api/' })
+
+    const config = {
+        baseUrl: baseUrl,
+        apiVersion: apiVersion
+    }
+
+    ReactDOM.render(
+        <App config={config} appName={appName} />,
+        document.getElementById("root")
+    )
+}
+
+main()
