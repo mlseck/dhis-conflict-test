b500054d3e4d1a0f2aff27a1554d9fbd7d9d7730
“Adja <m.seck@melotek.co>
2023-04-13 12:01:45 +0000

    Testing rendering of index page

--- src/index.js
+++ src/index.js
@@ -1,42 +1,7 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { init } from 'd2'
-import axios from 'axios'
-import './index.css'
-import App from './components/App'
-import * as serviceWorker from './serviceWorker'
-import dotenv from 'dotenv'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
-import 'typeface-roboto'
+const myFirstElement = <h1>Hello React!</h1>
 
-dotenv.config()
-const appName = process.env.REACT_APP_DHIS2_APP_NAME
-const apiVersion = process.env.REACT_APP_DHIS2_API_VERSION
-
-export async function getBaseUrl() {
-  if (process.env.NODE_ENV === "development") {
-      const baseUrl = process.env.REACT_APP_DHIS2_BASE_URL || "http://localhost:8080"
-      console.info(`[DEV] DHIS2 instance: ${baseUrl}`)
-      return baseUrl.replace(/\/*$/, "")
-  } else {
-      const { data: manifest } = await axios.get("manifest.webapp")
-      return manifest.activities.dhis.href
-  }
-}
-async function main() {
-  const baseUrl = await getBaseUrl()
-  init({ baseUrl: baseUrl + '/api/' })
-
-  const config = {
-    baseUrl: baseUrl,
-    apiVersion: apiVersion
-  }
-
-  ReactDOM.render(
-    <App config={ config } appName={ appName }/>,
-    document.getElementById("root")
-  )
-  serviceWorker.unregister()
-}
-
-main()
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(myFirstElement);
